4fbe963a13379e95d260e5631ad7d0fc02cdd083
Nathan Pownell <dirato2000@gmail.com>
2018-01-18 16:51:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return "I love you, too."
+}
